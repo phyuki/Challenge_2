b9bbd724efb6d38c3113231fbca56c37a092bcc5
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-07-28 12:18:08 -0300

    Created "StudentService" class

---
+++ src/main/java/br/com/compass/challenge2/service/StudentService.java
@@ -0,0 +1,52 @@
+package br.com.compass.challenge2.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.compass.challenge2.entity.Student;
+import br.com.compass.challenge2.repository.StudentRepository;
+
+public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById() {
+        return studentRepository.findById(null).orElseThrow(NoSuchElementException::new);
+    }
+
+    public Student findOne() {
+        return studentRepository.findOne(null).orElseThrow(NoSuchElementException::new);
+    }
+
+    public Student update(Student student) {
+
+        if (studentRepository.existsById(student.getId()))
+            return studentRepository.save(null);
+
+        throw new IllegalArgumentException();
+    }
+
+    public void delete(Student student) {
+        studentRepository.delete(student);
+    }
+
+    public void deleteById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
