00739edb705809aa7b0e1c879825de2620974c7f
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-07-28 15:03:29 -0300

    Created StudentController

--- src/main/java/br/com/compass/challenge2/controller/StudentController.java
+++ src/main/java/br/com/compass/challenge2/controller/StudentController.java
@@ -1,29 +1,83 @@
 package br.com.compass.challenge2.controller;
 
-import br.com.compass.challenge2.entity.Assessment;
-import br.com.compass.challenge2.entity.Student;
-import br.com.compass.challenge2.service.AssessmentService;
-import br.com.compass.challenge2.service.StudentService;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.compass.challenge2.entity.Student;
+import br.com.compass.challenge2.service.StudentService;
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/students")
 public class StudentController {
-    private  StudentService studentService;
+
+    private final StudentService studentService;
+
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @PostMapping
-    public ResponseEntity<Student> createStudent(@Valid @RequestBody Student studente) {
-        Student createdStudent = studentService.save(studente);
+    public ResponseEntity<Student> createStudent(@Valid @RequestBody Student student) {
+        Student createdStudent = studentService.save(student);
         return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> findAllStudents() {
+        List<Student> students = studentService.findAll();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> findStudentById(@PathVariable Long id) {
+        Student student = studentService.findById(id);
+
+        if (student != null) {
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@Valid @PathVariable Long id,
+            @RequestBody Student student) {
+
+        Student existingStudent = studentService.findById(id);
+
+        if (student != null) {
+            student.setId(id);
+            Student updatedStudent = studentService.update(existingStudent);
+            return new ResponseEntity<>(updatedStudent, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        Student existingStudent = studentService.findById(id);
+
+        if (existingStudent != null) {
+            studentService.deleteById(id);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
