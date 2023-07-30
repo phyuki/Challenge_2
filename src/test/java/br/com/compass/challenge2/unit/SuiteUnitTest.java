fdd20db08a43c3f94cba61c19f42b1e7d238e615
Ezequias Soares <87997012+kia735@users.noreply.github.com>
2023-07-30 14:52:56 -0300

    add suites

--- src/test/java/br/com/compass/challenge2/unit/SuiteUnitTest.java
+++ src/test/java/br/com/compass/challenge2/unit/SuiteUnitTest.java
@@ -1,4 +1,14 @@
 package br.com.compass.challenge2.unit;
 
+import br.com.compass.challenge2.unit.service.SuiteService;
+import org.junit.platform.commons.annotation.Testable;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@Testable
+@SelectClasses({
+        SuiteService.class
+})
 public class SuiteUnitTest {
 }
