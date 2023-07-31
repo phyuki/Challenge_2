03a29aebb96ee096b1196c4382f4e3ca452ecce4
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-07-31 00:38:39 -0300

    Fix "Equals" and "HashCode"

--- src/main/java/br/com/compass/challenge2/entity/Student.java
+++ src/main/java/br/com/compass/challenge2/entity/Student.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.hateoas.RepresentationModel;
 
@@ -42,58 +44,17 @@ public class Student extends RepresentationModel<Student> {
     @JsonIgnore
     private List<Assessment> assessments;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((group == null) ? 0 : group.hashCode());
-		result = prime * result + ((squad == null) ? 0 : squad.hashCode());
-		result = prime * result + ((assessments == null) ? 0 : assessments.hashCode());
-		return result;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(group, student.group) && Objects.equals(squad, student.squad) && Objects.equals(assessments, student.assessments);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (group == null) {
-			if (other.group != null)
-				return false;
-		} else if (!group.equals(other.group))
-			return false;
-		if (squad == null) {
-			if (other.squad != null)
-				return false;
-		} else if (!squad.equals(other.squad))
-			return false;
-		if (assessments == null) {
-			if (other.assessments != null)
-				return false;
-		} else if (!assessments.equals(other.assessments))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, squad, assessments);
+    }
 }
